290d9d0462227c34a21910168a304ac924f4a78f
Gerald <gerald-thomas@github.com>
2013-08-28 22:53:13 +1000

    Scenario 2

--- features/step_definitions/merge_article_steps.rb
+++ features/step_definitions/merge_article_steps.rb
@@ -6,6 +6,15 @@ Then /^I should see a "(.*?)" form field$/ do |arg1|
 end
 
 Then /^I should be able to enter a valid Article ID$/ do
-  @like_article_id= page.find_labelled 'like_article_id'
+	#File.open(‘tmp/test.html’,’w’){|file| file.write (response.body)} `firefox ‘/tmp/test.html’`
+	#File.open('/tmp/test.html','w'){|file| file.write(response.body)} firefox `'/tmp/test.html'`
+	fill_in 'like_article_id_excerpt', :with => '2'
+	find_field('like_article_id_excerpt').value.should eq '2'
+  	#@like_article_id= page.find_field 'like_article_id_excerpt'
+  	# unless Article.find_by_id(@like_article_id).valid? then
+  	# 	#page.should have_content(:warning "Invalid Article ID")
+  	# 	assert page.has_content?("#{field.titlecase} #{error_message}")
+  	# end
+
 
 end
